afd32b41ad4bd59b0ee277460d8f66bd2e564955
alex <alcebaeza@gmail.com>
2019-07-02 15:44:49 -0400

    ya

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   
   devise_for :users
-  
+  resources :companies do
+    resources :complaints, only: :create
+  end
+
+  get 'modal_edit', to: 'companies#modal_edit'
+
+  root 'companies#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
